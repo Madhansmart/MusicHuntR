c2a863afb959f8fcca6abd25184ecbad9c327e34
Madhansmart <madhansmart7404@gmail.com>
2019-06-17 12:34:00 +0530

    Updates

---
+++ precache-manifest.ddde007f60014ddaa4ef793266b8e002.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8d7bd1081cb26e194b9a8384045eb2",
+    "url": "/MusicHunt/index.html"
+  },
+  {
+    "revision": "d72d236acd4a6e4d534a",
+    "url": "/MusicHunt/static/css/2.427492a1.chunk.css"
+  },
+  {
+    "revision": "d09ad8cef130a5ce3202",
+    "url": "/MusicHunt/static/css/main.f2e669ae.chunk.css"
+  },
+  {
+    "revision": "d72d236acd4a6e4d534a",
+    "url": "/MusicHunt/static/js/2.bdcaa962.chunk.js"
+  },
+  {
+    "revision": "d09ad8cef130a5ce3202",
+    "url": "/MusicHunt/static/js/main.fe716cd3.chunk.js"
+  },
+  {
+    "revision": "d8ecfffd8061365259db",
+    "url": "/MusicHunt/static/js/runtime~main.ecd0133a.js"
+  },
+  {
+    "revision": "806430aaaee91d74f89d8fd547cb2fed",
+    "url": "/MusicHunt/static/media/logo.806430aa.png"
+  }
+]);
